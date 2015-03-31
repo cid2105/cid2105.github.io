f18e2b5136b2f4108f28fc298aa013b364eb202b
cid2105 <cole.ian.diamond@gmail.com>
2015-03-31 01:19:40 -0400

    asdf

--- web/contact_form.php
+++ web/contact_form.php
@@ -1,8 +1,18 @@
 <?php
+
+require 'vendor/autoload.php';
+
 $to = "cole.ian.diamond@gmail.com";
 $subject = "[colediamond.com] Message from " . $_POST["name"];
 $txt = $_POST["message"];
 $headers = "From: " . $_POST["from"]
 
-mail($to,$subject,$txt,$headers);
+$sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
+$message = new SendGrid\Email();
+$message->addTo('cole.ian.diamond@gmail.com')->
+          setFrom($from)->
+          setSubject($subject)->
+          setText($txt)->
+          setHtml($txt);
+$response = $sendgrid->send($message);
 ?>
