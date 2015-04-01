fe212b1e55b5bb670203573e2090f86d1382872b
cid2105 <cole.ian.diamond@gmail.com>
2015-03-31 21:28:06 -0400

    asdf

--- contact_form.php
+++ contact_form.php
@@ -6,7 +6,7 @@ $from = $_POST['email'];
 $subject = "[colediamond.com] Message from {$_POST['name']} ({$from})";
 $text = $_POST['message'];
 
-$sendgrid = new SendGrid(get_env('SENDGRID_USERNAME'), get_env('SENDGRID_PASSWORD'));
+$sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
 
 $message = new SendGrid\Email();
 $message->addTo($to)->
